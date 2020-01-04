f3e38e2d1248df001a65c69d625fb6feb1b6b285
Ashwini-Kumar01 <32567431+Ashwini-Kumar01@users.noreply.github.com>
2020-01-04 21:06:57 +0530

    Update HelloWorld.java
    semi colon removed

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Ashwini");
-		System.out.println("Hello Dnyanda Kulkarni");
+		System.out.println("Hello Dnyanda Kulkarni")
 	}
 
 }
