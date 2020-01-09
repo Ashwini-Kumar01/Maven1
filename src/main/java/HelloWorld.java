b50c4e99c9a51e3d4d81288c7176d0b4166d466b
Ashwini-Kumar01 <32567431+Ashwini-Kumar01@users.noreply.github.com>
2020-01-09 09:05:30 +0530

    Update HelloWorld.java
    Create a file

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,6 +4,23 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Ashwini");
 		System.out.println("Hello Dnyanda Kulkarni");
+		
+			     File file = new File("C:\\newfile.txt");
+	     /*If file gets created then the createNewFile() 
+	      * method would return true or if the file is 
+	      * already present it would return false
+	      */
+             boolean fvar = file.createNewFile();
+	     if (fvar){
+	          System.out.println("File has been created successfully");
+	     }
+	     else{
+	          System.out.println("File already present at the specified location");
+	     }
+    	} catch (IOException e) {
+    		System.out.println("Exception Occurred:");
+	        e.printStackTrace();
+	  }
 	}
 
 }
